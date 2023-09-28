a0ca391b73c9bd1af71703a37c8ff7a575d8d0c4
RaphaelNdonga <nkibi53@gmail.com>
2023-09-28 12:58:37 +0300

    used cors library instead

--- main.go
+++ main.go
@@ -1,36 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		fmt.Println(c.Request.Method)
-
-		if c.Request.Method == "OPTIONS" {
-			fmt.Println("OPTIONS")
-			c.AbortWithStatus(204)
-		} else {
-			c.Next()
-		}
-	}
-}
 func main() {
 	router := gin.Default()
 
-	router.Use(CORSMiddleware())
-
+	router.Use(cors.Default())
 	router.GET("/test", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "test ok",
