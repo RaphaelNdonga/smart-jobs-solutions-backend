aca8e28a3a9cf46f688317bfe51a72ce89c3da58
RaphaelNdonga <nkibi53@gmail.com>
2023-09-28 19:28:40 +0300

    extracted postsignup

--- main.go
+++ main.go
@@ -15,19 +15,22 @@ type UserDetails struct {
 	UserType string `json:"userType"`
 }
 
+func postSignUp(ctx *gin.Context) {
+	userDetails := UserDetails{}
+	if err := ctx.BindJSON(&userDetails); err != nil {
+		fmt.Println("POST sign-up: bind json error")
+		return
+	}
+	fmt.Print("success: ", userDetails)
+	ctx.IndentedJSON(http.StatusOK, userDetails)
+}
+
 func main() {
 	router := gin.Default()
 
 	router.Use(cors.Default())
 
-	router.POST("/sign-up", func(ctx *gin.Context) {
-		userDetails := UserDetails{}
-		if err := ctx.BindJSON(&userDetails); err != nil {
-			fmt.Println("POST sign-up: bind json error")
-			return
-		}
-		fmt.Print("success: ", userDetails)
-		ctx.IndentedJSON(http.StatusOK, userDetails)
-	})
-	router.Run("localhost:8000")
+	router.POST("/sign-up", postSignUp)
+
+	router.Run(":8000")
 }
