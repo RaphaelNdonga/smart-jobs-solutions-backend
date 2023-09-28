6fc6a0f961d8288c1df61d6b9db133084958241d
RaphaelNdonga <nkibi53@gmail.com>
2023-09-28 19:22:58 +0300

    user details received from the client side

--- main.go
+++ main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+type UserDetails struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	UserType string `json:"userType"`
+}
+
 func main() {
 	router := gin.Default()
 
 	router.Use(cors.Default())
-	router.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "test ok",
-		})
-	})
 
 	router.POST("/sign-up", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "sign up received",
-		})
+		userDetails := UserDetails{}
+		if err := ctx.BindJSON(&userDetails); err != nil {
+			fmt.Println("POST sign-up: bind json error")
+			return
+		}
+		fmt.Print("success: ", userDetails)
+		ctx.IndentedJSON(http.StatusOK, userDetails)
 	})
 	router.Run("localhost:8000")
 }
