26eb7482cd4dffecb9b1876874efe52bcc731435
RaphaelNdonga <nkibi53@gmail.com>
2023-10-21 18:33:10 +0300

    extracted database pointer for testing

--- database/main.go
+++ database/main.go
@@ -10,19 +10,24 @@ import (
 
 var db *sql.DB
 
-func InitDB() {
+func InitDB() *sql.DB {
 	connStr := "user=raphaelndonga dbname=smartjobsolutions sslmode=disable"
 	conn, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	db = conn
+	return db
 }
 
-func AddUser(userDetails types.UserDetailsDB) {
+func GetDB() *sql.DB {
+	return db
+}
+
+func AddUser(db *sql.DB, userDetails types.UserDetailsDB) error {
 	pingErr := db.Ping()
 	if pingErr != nil {
-		log.Fatal(pingErr)
+		return pingErr
 	}
 	query := `
 		INSERT INTO userDetails VALUES (
@@ -33,9 +38,39 @@ func AddUser(userDetails types.UserDetailsDB) {
 		)
 	`
 
-	_, queryErr := db.Query(query, userDetails.UserType, userDetails.Email, userDetails.HashedPassword, userDetails.UserType)
+	_, queryErr := db.Exec(query, userDetails.Username, userDetails.Email, userDetails.HashedPassword, userDetails.UserType)
 
 	if queryErr != nil {
-		log.Fatal(queryErr)
+		return queryErr
 	}
+	return nil
+}
+
+func GetUserByEmail(db *sql.DB, email string) (types.UserDetailsDB, error) {
+	userDetails := types.UserDetailsDB{}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	query := `
+		SELECT * FROM userdetails WHERE email = $1	
+	`
+
+	rows, err := db.Query(query, email)
+	if err != nil {
+		return userDetails, err
+	}
+	for rows.Next() {
+		err := rows.Scan(
+			&userDetails.Username,
+			&userDetails.Email,
+			&userDetails.HashedPassword,
+			&userDetails.UserType,
+		)
+		if err != nil {
+			return userDetails, err
+		}
+	}
+	return userDetails, err
 }
