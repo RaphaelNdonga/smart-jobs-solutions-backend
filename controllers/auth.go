a21e830aaab265ea0271426e015a7895cf66e43a
RaphaelNdonga <nkibi53@gmail.com>
2023-10-22 20:30:58 +0300

    log.fatal removed

--- controllers/auth.go
+++ controllers/auth.go
@@ -12,19 +12,16 @@ import (
 )
 
 func SignUp(ctx *gin.Context) {
-	fmt.Println("sign up called...")
 	userDetails := types.UserDetails{}
 	if err := ctx.BindJSON(&userDetails); err != nil {
 		errorMsg := "POST sign-up: bind json error"
 		ctx.IndentedJSON(http.StatusInternalServerError, errorMsg)
-		log.Fatal("post signup error: ", err)
 	}
 	password := userDetails.Password
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 0)
 	if err != nil {
 		errorMsg := "POST sign-up: hashing err"
 		ctx.IndentedJSON(http.StatusInternalServerError, errorMsg)
-		log.Fatal(err)
 	}
 	userDetailsDB := types.UserDetailsDB{
 		Username:       userDetails.Username,
