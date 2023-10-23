90c01c38d50e00a3176597650b67e7b21b97b4ce
RaphaelNdonga <nkibi53@gmail.com>
2023-10-23 08:44:32 +0300

    removed initDB from setting up router becuase it doesn't make sense

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"smartjobsolutions/database"
 	"smartjobsolutions/routes"
 )
 
 func main() {
-
+	database.InitDB()
 	router := routes.SetupRouter()
 
 	router.Run(":8000")
--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"smartjobsolutions/controllers"
-	"smartjobsolutions/database"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -11,7 +10,6 @@ import (
 var router *gin.Engine
 
 func SetupRouter() *gin.Engine {
-	database.InitDB()
 	router = gin.Default()
 	router.Use(cors.Default())
 	router.POST("/sign-up", controllers.SignUp)
