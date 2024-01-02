77e601e328d1480d576a243102ce8848a676af79
RaphaelNdonga <nkibi53@gmail.com>
2024-01-02 12:58:56 +0300

    using separate likes table

--- database/posts.go
+++ database/posts.go
@@ -4,8 +4,8 @@ import "database/sql"
 
 func LikePost(db *sql.DB, userId string, postId string) error {
 	query := `
-		UPDATE posts SET liked_by = ARRAY_APPEND(liked_by, $1) WHERE id = $2	
+		INSERT INTO likes VALUES ($1, $2)
 	`
-	_, err := db.Exec(query, userId, postId)
+	_, err := db.Exec(query, postId, userId)
 	return err
 }
